72fdedb43b1f7ced2112584372d07f31f2f12b44
auswitch <sawit_cha@cmu.ac.th>
2022-02-02 12:44:18 +0700

    enemy

---
+++ Game/Game/Enemy/Game.h
@@ -0,0 +1,66 @@
+
+#include<iostream>
+#include<vector>
+#include<ctime>
+
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/Network.hpp>
+
+/*
+    Class that acts as the game engine.
+    Wrapper class.
+*/
+
+class Game
+{
+private:
+    //Variables
+    //Window
+    sf::RenderWindow* window;
+    sf::VideoMode videoMode;
+    sf::Event ev;
+
+    //Mouse positions
+    sf::Vector2i mousePosWindow;
+
+    //Game logic
+    int points;
+    float enemySpawnTimer;
+    float enemySpawnTImerMax;
+    int maxEnemies;
+
+    //Game objects
+    std::vector<sf::RectangleShape> enemies;
+    sf::RectangleShape enemy;
+
+    //Private functions
+    void initVariables();
+    void initWindow();
+    void initEnemies();
+public:
+    //Constructors / Destructors
+    Game();
+    virtual ~Game();
+
+
+    //Accessors
+    const bool running() const;
+
+    //Functions
+    void spawnEnemy();
+    void pollvents();
+    void updateMousePositions();
+    void updateEnemies();
+    void update();
+
+    void renderEnemies();
+    void render();
+
+
+    //Game objects
+    sf::RectangleShape enemy;
+}
+
