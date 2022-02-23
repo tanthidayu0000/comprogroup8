8b1786d184320b1b81ed18a40bdbe90be209d925
auswitch <sawit_cha@cmu.ac.th>
2022-02-23 17:25:28 +0700

    UpdateChangemap

--- Game/Game/Map1.cpp
+++ Game/Game/Map1.cpp
@@ -9,13 +9,14 @@ void Map1::initVariables()
 	{
 		throw("ERROR::MAP1::CLOUD NOT LOAD FONT");
 	}
-
+	
 	this->text.setCharacterSize(20);
 	this->text.setFont(this->font);
 	this->text.setColor(Color::White);
 	this->text.setOutlineColor(Color::Black);
 	this->text.setOutlineThickness(1);
 	this->text.setString("You must pick up all coins and open box.");
+	this->text.setPosition(this->text.getGlobalBounds().width/2, this->text.getGlobalBounds().height/2);
 
 	this->background.setSize(
 		Vector2f(
